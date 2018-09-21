a6e439ea36c2faa1e16f598b48d16942b76f880e
GODSKADI <aidrean.gf@gmail.com>
2018-09-21 17:16:01 +0200

    second

---
+++ Ex_Variables_Primitius/Ex3.php
@@ -0,0 +1,12 @@
+<?php
+$i = 12;
+$tipus_de_i = gettype( $i );
+echo "La variable \$i
+      conté el valor $i
+      i és del tipus $tipus_de_i";
+
+echo "<br>\n
+      <br>\n 3. Explica perquè hem posat la barra invertida davant del símbol $.
+      <br>\n Ponemos la \ delante del simbolo $, para que no coja el resultado de la variable que hemos definido anteriormente.";
+
+?>
---
+++ Ex_Variables_Primitius/Ex5.php
@@ -0,0 +1,11 @@
+<?php
+$i = 12;
+$tipus_de_i = gettype( $i );
+echo "La variable \$i
+      conté el valor $i
+      i és del tipus $tipus_de_i";
+
+echo "<br>
+      <br>\n 5. Digues per a que serveix la funció gettype.
+      <br>\n · Sirve para obtener el tipo de una variable.";
+?>
