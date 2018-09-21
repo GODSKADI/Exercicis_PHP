297e29ba2fc89855411e6b8340a440ed71fe70b3
GODSKADI <aidrean.gf@gmail.com>
2018-09-21 19:03:19 +0200

    segunda ronda de ejercicios

--- El_tipus_objecte/Ex6.php
+++ El_tipus_objecte/Ex6.php
@@ -1,7 +1,7 @@
 <?php
 $d = new DateTime();
 $tipus_de_d = gettype( $d );
-$tipus_de_gd = gettype($tipus_de_d);
+$tipus_de_gd = get_class($d);
 echo "La variable \$d
       conté el valor " . $d->format( "d/m/Y") .
       " i és del tipus $tipus_de_d.";
